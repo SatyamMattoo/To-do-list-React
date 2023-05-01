baf5cd599c895883f39f63fa95761cad6cac8f18
SatyamMattoo <satyammattoo2003gmail.com>
2023-05-02 03:02:51 +0530

    first

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Task({title, description, deleteTask2, index}) {
+function Task({title, description, deleteTask, index}) {
   return (
     <div className="task">
         <div className="output">
             <p>{title}</p>
             <span>{description}</span>
         </div>
-        <button onClick={() => deleteTask2(index)}>-</button>
+        <button onClick={() => deleteTask(index)}>-</button>
     </div>
   )
 }
